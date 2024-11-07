613c8b3083a7d52f77e4b06747008ffad1ac2020
Armand Kassaï Koupaï <armandkassaikoupai@gmail.com>
2024-11-07 15:43:42 +0100

    Update adapt.sh

--- bash/model/adapt.sh
+++ bash/model/adapt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=fuels
-#SBATCH --partition=funky
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=10000
-#SBATCH --output=slurm_run/fuels_adapt/%x-%j.out
-#SBATCH --error=slurm_run/fuels_adapt/%x-%j.err
+#SBATCH --job-name=xxx
+#SBATCH --partition=xxx
+#SBATCH --nodes=xxx
+#SBATCH --gpus-per-node=xxx
+#SBATCH --time=xxx
+#SBATCH --output=xxx
+#SBATCH --error=xxx
 
 source $MINICONDA_PATH/etc/profile.d/conda.sh
 
@@ -13,19 +13,19 @@ set -x
 conda init bash
 conda activate fuels
 
-dataset_name='combined_big' # pendulum, burgers, gs, lv
-batch_size_train=32 # 4 if lv, 16 if pendulum, 1 if gs, 4 if burgers
+dataset_name='xxx'
+batch_size_train=32
 batch_size_val=32
 epochs=1000
 lr=0.01
 seed=123
 hidden_c=16
-state_c=1 # 2 others, 1 if burgers
+state_c=1
 code_c=8
 is_complete=True
 type_augment=''
-factor=1 # 1 if lv or pendulum, 0.0005 if gs
-run_name='cerulean-tree-2738'
+factor=1
+run_name='xxx'
 
 python3 adapt.py "data.dataset_name=$dataset_name" "optim.batch_size_train=$batch_size_train" "optim.batch_size_val=$batch_size_val" "data.seed=$seed" "optim.epochs=$epochs" "optim.lr=$lr"  "model.hidden_c=$hidden_c" "model.state_c=$state_c" "model.code_c=$code_c" "pretrain.run_name=$run_name" "model.factor=$factor" "model.is_complete=$is_complete" "model.type_augment=$type_augment"
 
